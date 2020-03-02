25fdf768b21611feeac5673285f5b3a8e46a3320
Akari Rin <3311453797@qq.com>
2020-03-02 10:10:21 +0800

    加入mdui

--- src/entry/400.js
+++ src/entry/400.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import 'mdui/dist/css/mdui.css'
 import httpcode from '../layouts/httpcode.vue'
 
 Vue.config.productionTip = false
--- src/entry/noprod.js
+++ src/entry/noprod.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import 'mdui/dist/css/mdui.css'
 import prodlint from '../layouts/prodlint.vue'
 
 Vue.config.productionTip = false
